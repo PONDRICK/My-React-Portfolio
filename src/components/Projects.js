cea20646c579dc8d044c506e5a7ec362abea2c6d
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-04-17 16:53:47 +0700

    update project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,8 @@ import image_country from "../assets/images/Country_API.png";
 import image_memo from "../assets/images/MemoAPP.png";
 import image_covid from "../assets/images/Covid.png";
 import image_sucide from "../assets/images/Sucide_rate.png";
+import image_rabbit from "../assets/images/rabbit_card.jpg";
+import image_seek from "../assets/images/Seek_campaign.png";
 
 function Projects() {
   return (
@@ -58,12 +60,30 @@ function Projects() {
             <p>2023</p>
           </a>
           <a
+            href="https://docs.google.com/document/d/1YddJAZqO_BAP_oZp8C8NTAHsDHTjajtrFxzO_KDd9Tk/edit?usp=sharing"
+            target="_blank"
+            class="project-card"
+          >
+            <img src={image_seek} />
+            <h3>Seek Campaign System Analysis Design</h3>
+            <p>2023</p>
+          </a>
+          <a
             href="https://github.com/PONDRICK/Suicide-rate-Project"
             target="_blank"
             class="project-card"
           >
             <img src={image_sucide} />
             <h3>Suicide Rate Prediction Mini Project</h3>
+            <p>2022</p>
+          </a>
+          <a
+            href="https://docs.google.com/document/d/1xiywLjA7Zh1-uK9Vrn61E-vTeqstQ7NJVTLVwP5SMWY/edit?usp=sharing"
+            target="_blank"
+            class="project-card"
+          >
+            <img src={image_rabbit} />
+            <h3>Rabbit Card Database Design</h3>
             <p>2022</p>
           </a>
         </div>
