da91b6c8db9b7834613cc6d7efd2cb04b363b6ba
PONDRICK <99117013+PONDRICK@users.noreply.github.com>
2024-07-30 17:55:01 +0700

    update skill and project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,10 @@ import image_rabbit from "../assets/images/rabbit_card.jpg";
 import image_seek from "../assets/images/Seek_campaign.png";
 import Rabbit_pdf from "../assets/images/Rabbit.pdf";
 import Seek_pdf from "../assets/images/ปัญหาการส่งเสริมสุขภาพ.pdf";
+import image_kidney from "../assets/images/Kidney.png";
+import image_janken from "../assets/images/Janken.png";
+import image_spotscout from "../assets/images/spotscout.png";
+import image_spot from "../assets/images/spot.png";
 
 function Projects() {
   return (
@@ -16,6 +20,33 @@ function Projects() {
       <div class="content">
         <h2>Highlight Projects</h2>
         <div class="projects-grid">
+          <a
+            href="https://github.com/PONDRICK/Spot_Scout"
+            target="_blank"
+            class="project-card"
+          >
+            <img src={image_spotscout} />
+            <h3>Spotscout Final Project</h3>
+            <p>2024</p>
+          </a>
+          <a
+            href="https://github.com/PONDRICK/Place_Predict"
+            target="_blank"
+            class="project-card"
+          >
+            <img src={image_spot} />
+            <h3>Research on Models Predicting Place Types</h3>
+            <p>2024</p>
+          </a>
+          <a
+            href="https://github.com/PONDRICK/Kidney_Stone_Detection"
+            target="_blank"
+            class="project-card"
+          >
+            <img src={image_kidney} />
+            <h3>Kidney Stone Detection</h3>
+            <p>2023</p>
+          </a>
           <a
             href="https://github.com/PONDRICK/My-React-Portfolio"
             target="_blank"
@@ -26,39 +57,12 @@ function Projects() {
             <p>2023</p>
           </a>
           <a
-            href="https://github.com/PONDRICK/Food-Menu-Web"
-            target="_blank"
-            class="project-card"
-          >
-            <img src={image_food} />
-            <h3>Food Menu</h3>
-            <p>2023</p>
-          </a>
-          <a
-            href="https://github.com/PONDRICK/Country-API.github.io"
-            target="_blank"
-            class="project-card"
-          >
-            <img src={image_country} />
-            <h3>Country API</h3>
-            <p>2023</p>
-          </a>
-          <a
             href="https://github.com/PONDRICK/MeMoStar"
             target="_blank"
             class="project-card"
           >
             <img src={image_memo} />
             <h3>Memo Mobile Application Mini Project</h3>
-            <p>2023</p>
-          </a>
-          <a
-            href="https://github.com/PONDRICK/Covid"
-            target="_blank"
-            class="project-card"
-          >
-            <img src={image_covid} />
-            <h3>Covid Mobile Application Mini Project</h3>
             <p>2023</p>
           </a>
           <a href={Seek_pdf} target="_blank" class="project-card">
@@ -73,6 +77,15 @@ function Projects() {
           >
             <img src={image_sucide} />
             <h3>Suicide Rate Prediction Mini Project</h3>
+            <p>2022</p>
+          </a>
+          <a
+            href="https://github.com/PONDRICK/JangKenFlightJavaMiniProject"
+            target="_blank"
+            class="project-card"
+          >
+            <img src={image_janken} />
+            <h3>JangKenFight Java Mini Project</h3>
             <p>2022</p>
           </a>
           <a href={Rabbit_pdf} target="_blank" class="project-card">
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,24 +1,112 @@
 import React from "react";
+import {
+  DiPython,
+  DiJava,
+  DiJavascript1,
+  DiReact,
+  DiAngularSimple,
+  DiNodejsSmall,
+  DiDocker,
+  DiHtml5,
+  DiCss3,
+  DiDatabase,
+  DiPostgresql,
+  DiDjango,
+  DiMysql,
+} from "react-icons/di";
 
 function Skills() {
   return (
     <section id="skills" className="section">
       <div className="content">
         <h2>Skills</h2>
-        <ul className="skills-grid">
-          <li>Python</li>
-          <li>Java</li>
-          <li>Javascript</li>
-          <li>C</li>
-          <li>MATLAB</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>React</li>
-          <li>React Native</li>
-          <li>Node.js</li>
-          <li>MySQL</li>
-          <li>Docker</li>
-        </ul>
+
+        <div className="skills-category">
+          <h3>Programming Languages</h3>
+          <ul className="skills-grid">
+            <li>
+              <DiPython /> Python
+            </li>
+            <li>
+              <DiJava /> Java
+            </li>
+            <li>
+              <DiJavascript1 /> Javascript
+            </li>
+            <li>
+              <DiJavascript1 /> Typescript
+            </li>
+            <li>
+              <DiJavascript1 /> C
+            </li>
+            <li>
+              <DiJavascript1 /> C#
+            </li>
+            <li>
+              <DiJavascript1 /> MATLAB
+            </li>
+          </ul>
+        </div>
+
+        <div className="skills-category">
+          <h3>Frontend</h3>
+          <ul className="skills-grid">
+            <li>
+              <DiHtml5 /> HTML
+            </li>
+            <li>
+              <DiCss3 /> CSS
+            </li>
+            <li>
+              <DiReact /> React
+            </li>
+            <li>
+              <DiReact /> React Native
+            </li>
+            <li>
+              <DiAngularSimple /> Angular
+            </li>
+          </ul>
+        </div>
+
+        <div className="skills-category">
+          <h3>Backend</h3>
+          <ul className="skills-grid">
+            <li>
+              <DiNodejsSmall /> Node.js
+            </li>
+            <li>
+              <DiNodejsSmall /> Express.js
+            </li>
+            <li>
+              <DiDjango /> Django
+            </li>
+          </ul>
+        </div>
+
+        <div className="skills-category">
+          <h3>Database</h3>
+          <ul className="skills-grid">
+            <li>
+              <DiMysql /> MySQL
+            </li>
+            <li>
+              <DiPostgresql /> PostgreSQL
+            </li>
+          </ul>
+        </div>
+
+        <div className="skills-category">
+          <h3>Tools</h3>
+          <ul className="skills-grid">
+            <li>
+              <DiDocker /> Docker
+            </li>
+            <li>
+              <DiDatabase /> Postman
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
